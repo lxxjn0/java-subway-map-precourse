6a165ba7b964db05f899f385d0156ea560f858c0
lxxjn0 <ljy3510@gmail.com>
2020-12-09 18:13:58 +0900

    feat(factory): 초기 설정된 StationRepository를 만드는 기능 구현
    - StationRepositoryFactory 클래스 구현
    - StationRepositoryFactoryTest 작성

---
+++ src/main/java/subway/infra/factory/StationRepositoryFactory.java
@@ -0,0 +1,44 @@
+package subway.infra.factory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import subway.domain.Station;
+import subway.domain.StationRepository;
+
+public class StationRepositoryFactory {
+    static final Station 교대역 = new Station("교대역");
+    static final Station 강남역 = new Station("강남역");
+    static final Station 역삼역 = new Station("역삼역");
+    static final Station 남부터미널역 = new Station("남부터미널역");
+    static final Station 양재역 = new Station("양재역");
+    static final Station 양재시민의숲역 = new Station("양재시민의숲역");
+    static final Station 매봉역 = new Station("매봉역");
+    private static final List<Station> initializedStations = new ArrayList<>();
+
+    static {
+        final List<Station> stations = Arrays.asList(
+                교대역,
+                강남역,
+                역삼역,
+                남부터미널역,
+                양재역,
+                양재시민의숲역,
+                매봉역
+        );
+        initializedStations.addAll(stations);
+    }
+
+    private StationRepositoryFactory() {
+    }
+
+    public static StationRepository generate() {
+        final StationRepository stationRepository = new StationRepository();
+
+        for (Station station : initializedStations) {
+            stationRepository.save(station);
+        }
+        return stationRepository;
+    }
+}
---
+++ src/test/java/subway/infra/factory/StationRepositoryFactoryTest.java
@@ -0,0 +1,24 @@
+package subway.infra.factory;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import subway.domain.StationRepository;
+
+class StationRepositoryFactoryTest {
+    @DisplayName("초기 설정된 StationRepository를 반환한다")
+    @Test
+    void generate() {
+        // When
+        final StationRepository stationRepository = StationRepositoryFactory.generate();
+
+        // Then
+        assertThat(stationRepository)
+                .extracting(StationRepository::findAll)
+                .asList()
+                .hasSize(7)
+        ;
+    }
+}
