123b4cc1c3bce0f7c0c2679ee831bf768046e32e
lxxjn0 <ljy3510@gmail.com>
2020-12-13 00:21:24 +0900

    feat(view): InputView 구현

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -2,9 +2,11 @@ package subway;
 
 import java.util.Scanner;
 
+import subway.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+        final InputView inputView = new InputView(scanner);
     }
 }
