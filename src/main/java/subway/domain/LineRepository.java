5e33f33c83680f58e0031148c7dc4c74bc92b3a3
lxxjn0 <ljy3510@gmail.com>
2020-12-12 23:35:35 +0900

    refactor(repository): Repository 인터페이스 제거

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -5,21 +5,18 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
-public class LineRepository implements Repository<Line, String> {
+public class LineRepository {
     private static final List<Line> lines = new ArrayList<>();
 
-    @Override
     public List<Line> findAll() {
         return Collections.unmodifiableList(lines);
     }
 
-    @Override
     public void save(Line line) {
         lines.add(line);
     }
 
-    @Override
-    public boolean delete(String name) {
+    public boolean deleteByName(String name) {
         return lines.removeIf(line -> Objects.equals(line.getName(), name));
     }
 }
--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -5,21 +5,18 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
-public class StationRepository implements Repository<Station, String> {
+public class StationRepository {
     private static final List<Station> stations = new ArrayList<>();
 
-    @Override
     public List<Station> findAll() {
         return Collections.unmodifiableList(stations);
     }
 
-    @Override
     public void save(Station station) {
         stations.add(station);
     }
 
-    @Override
-    public boolean delete(String name) {
+    public boolean deleteByName(String name) {
         return stations.removeIf(station -> Objects.equals(station.getName(), name));
     }
 }
