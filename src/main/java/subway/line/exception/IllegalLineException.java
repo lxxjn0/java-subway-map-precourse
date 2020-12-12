422c49ca14eadf906d7b7b1fc31860d616a251fe
lxxjn0 <ljy3510@gmail.com>
2020-12-13 02:22:10 +0900

    refactor(exception): 유효하지 않은 역 이름, 노선 이름 예외 메시지 수정

--- src/main/java/subway/line/exception/IllegalLineException.java
+++ src/main/java/subway/line/exception/IllegalLineException.java
@@ -1,7 +1,7 @@
 package subway.line.exception;
 
 public class IllegalLineException extends IllegalArgumentException {
-    public static final String INVALID_NAME = "유효하지 않은 이름입니다.";
+    public static final String INVALID_NAME = "유효하지 않은 노선 이름입니다.";
 
     public IllegalLineException(final String s) {
         super(s);
--- src/main/java/subway/station/exception/IllegalStationException.java
+++ src/main/java/subway/station/exception/IllegalStationException.java
@@ -1,7 +1,7 @@
 package subway.station.exception;
 
 public class IllegalStationException extends IllegalArgumentException {
-    public static final String INVALID_NAME = "유효하지 않은 이름입니다.";
+    public static final String INVALID_NAME = "유효하지 않은 역 이름입니다.";
 
     public IllegalStationException(final String s) {
         super(s);
